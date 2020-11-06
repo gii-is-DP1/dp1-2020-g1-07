c880ed56a3fbed9ebb83543044623c4fe5b1ef23
Antonio González Gómez <antoniogg696@gmail.com>
2020-11-06 15:56:43 +0100

    Deleted test project and created a blank Spring project

---
+++ src/main/java/g107/casino/CasinoWebAppApplication.java
@@ -0,0 +1,13 @@
+package g107.casino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/g107/casino/CasinoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package g107.casino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasinoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
