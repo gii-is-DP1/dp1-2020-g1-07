91c1cd3c0790b54833ae2080a48077b193e47408
Daniel Caro <danielcaroolmedo2@gmail.com>
2020-11-14 18:45:12 +0100

    Validator added to dish
    Name not null

--- src/main/java/org/springframework/samples/petclinic/web/DishController.java
+++ src/main/java/org/springframework/samples/petclinic/web/DishController.java
@@ -13,7 +13,9 @@ import org.springframework.samples.petclinic.service.DishService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +27,11 @@ public class DishController {
 
 	@Autowired
 	private DishService dishService;
+	
+	@InitBinder("dish")
+	public void initDishBinder(WebDataBinder dataBinder) {
+		dataBinder.setValidator(new DishValidator());
+	}
 	
 	@GetMapping()
 	public String dishesList(ModelMap modelMap) {
---
+++ src/main/java/org/springframework/samples/petclinic/web/DishValidator.java
@@ -0,0 +1,30 @@
+package org.springframework.samples.petclinic.web;
+
+import org.springframework.samples.petclinic.model.Client;
+import org.springframework.samples.petclinic.model.Dish;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class DishValidator implements Validator {
+
+	private static final String REQUIRED = "required";
+
+	@Override
+	public void validate(Object obj, Errors errors) {
+		Dish dish = (Dish) obj;
+		String name = dish.getName();
+		// name validation
+		if (name == null || name.trim().equals("")) {
+			errors.rejectValue("name", REQUIRED, REQUIRED);
+		}
+	}
+
+	/**
+	 * This Validator validates *just* Pet instances
+	 */
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Dish.class.isAssignableFrom(clazz);
+	}
+
+}
