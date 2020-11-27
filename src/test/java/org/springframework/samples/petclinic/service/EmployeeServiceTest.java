ea51add3ab17207df33408d4cdedd648a766a0e0
Antonio González Gómez <antoniogg696@gmail.com>
2020-11-27 19:08:57 +0100

    Added "EmployeeService" tests

---
+++ src/test/java/org/springframework/samples/petclinic/service/EmployeeServiceTest.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.petclinic.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class EmployeeServiceTest {
+
+	@Autowired
+	private EmployeeService empService;
+	
+	@Test
+	public void testCountWithInitialData() {
+		int count= empService.employeeCount();
+		assertEquals(count,3);
+	}
+}
