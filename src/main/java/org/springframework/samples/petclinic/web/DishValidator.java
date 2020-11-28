621301cd2407499b5f31b041eba1e4fdfab63550
Vicente <visorvaz@gmail.com>
2020-11-28 21:58:27 +0100

    Si

--- src/main/java/org/springframework/samples/petclinic/web/DishValidator.java
+++ src/main/java/org/springframework/samples/petclinic/web/DishValidator.java
@@ -1,13 +1,13 @@
 package org.springframework.samples.petclinic.web;
 
-<<<<<<< Updated upstream
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.springframework.samples.petclinic.model.Dish;
 import org.springframework.samples.petclinic.service.DishService;
-=======
+
 
 
 import java.util.Locale;
@@ -19,20 +19,17 @@ import org.junit.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.samples.petclinic.model.Dish;
 import org.springframework.samples.petclinic.model.Person;
->>>>>>> Stashed changes
+
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 public class DishValidator implements Validator {
 
 	private static final String REQUIRED = "required";
-<<<<<<< Updated upstream
+
 	protected DishService dishService;
 	private Integer prueba = 0;
-=======
-	 
-	
->>>>>>> Stashed changes
+
 
 	public Dish getDishwithIdDifferent(String name, Integer id) {
 		name = name.toLowerCase();
@@ -67,10 +64,7 @@ public class DishValidator implements Validator {
 		return Dish.class.isAssignableFrom(clazz);
 	}
 	
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 	
 
 }
