1cef2eb0c6e6974d3af1d3247e22065768218997
Vicente <visorvaz@gmail.com>
2020-12-17 18:42:02 +0100

    Added jsp views to event controller

--- src/main/java/org/springframework/samples/petclinic/web/EventController.java
+++ src/main/java/org/springframework/samples/petclinic/web/EventController.java
@@ -9,12 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Artist;
 import org.springframework.samples.petclinic.model.Event;
 import org.springframework.samples.petclinic.model.ShowType;
-import org.springframework.samples.petclinic.model.Stage;
 import org.springframework.samples.petclinic.service.EventService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,24 +28,29 @@ public class EventController {
 	@Autowired
 	private EventService eventService;
 	
+	@InitBinder("event")
+	public void initMenuBinder(WebDataBinder dataBinder) {
+		dataBinder.setValidator(new EventValidator());
+	}
+	
 	@GetMapping()
-	public String eventsListed(ModelMap modelMap) {
-		String view = "stages/listEvent";
+	public String eventsList(ModelMap modelMap) {
+		String view = "events/listEvent";
 		Iterable<Event> events=eventService.findAll();
-		modelMap.addAttribute("stages", events);
+		modelMap.addAttribute("events", events);
 		return view;
 	}
 
 	@GetMapping(path="/new")
 	public String createEvent(ModelMap modelMap) {
-		String view="stages/addEvent";
+		String view="events/addEvent";
 		modelMap.addAttribute("event", new Event());
 		return view;
 	}
 	
 	@PostMapping(path="/save")
 	public String saveEvent(@Valid Event event, BindingResult result, ModelMap modelMap) {
-		String view="stages/listEvent";
+		String view="events/listEvent";
 		if(result.hasErrors()) {
 			modelMap.addAttribute("event", event);
 			return "events/addEvent";
@@ -54,21 +60,21 @@ public class EventController {
 			eventService.save(event);
 			
 			modelMap.addAttribute("message", "Event successfully saved!");
-			view=eventsListed(modelMap);
+			view=eventsList(modelMap);
 		}
 		return view;
 	}
 	@GetMapping(path="/delete/{eventId}")
 	public String deleteEvent(@PathVariable("eventId") int eventId, ModelMap modelMap) {
-		String view="stages/listEvent";
+		String view="events/listEvent";
 		Optional<Event> event = eventService.findEventbyId(eventId);
 		if(event.isPresent()) {
 			eventService.delete(event.get());
 			modelMap.addAttribute("message", "Event successfully deleted!");
-			view=eventsListed(modelMap);
+			view=eventsList(modelMap);
 		}else {
 			modelMap.addAttribute("message", "Event not found!");
-			view=eventsListed(modelMap);
+			view=eventsList(modelMap);
 		}
 		return view;
 	}
