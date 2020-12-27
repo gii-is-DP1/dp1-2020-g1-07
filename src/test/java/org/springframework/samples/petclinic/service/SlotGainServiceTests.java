fef21e5ab54dea3b78b895ed5379bf3ec6d4be3d
Bea <beltranalvarezbeatriz@gmail.com>
2020-12-27 13:37:39 +0100

    Slot Game Service Tests

--- src/test/java/org/springframework/samples/petclinic/service/SlotGainServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/SlotGainServiceTests.java
@@ -1,23 +1,45 @@
 package org.springframework.samples.petclinic.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+<<<<<<< Updated upstream
 import static org.junit.jupiter.api.Assertions.assertTrue;
+=======
+import static org.mockito.Mockito.when;
+>>>>>>> Stashed changes
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.petclinic.model.Dish;
+import org.springframework.samples.petclinic.model.DishCourse;
+import org.springframework.samples.petclinic.model.Shift;
 import org.springframework.samples.petclinic.model.SlotGain;
+<<<<<<< Updated upstream
+=======
+import org.springframework.samples.petclinic.model.SlotMachine;
+import org.springframework.samples.petclinic.model.Slotgame;
+import org.springframework.samples.petclinic.model.Status;
+import org.springframework.samples.petclinic.repository.DishRepository;
+import org.springframework.samples.petclinic.repository.SlotGainRepository;
+>>>>>>> Stashed changes
 import org.springframework.samples.petclinic.util.EntityUtils;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class SlotGainServiceTests {
+	
+	@Mock
+	private SlotGainRepository slotGainRepo;
+	
 	@Autowired
 	protected SlotGainService slotGainService;
 	
@@ -26,15 +48,57 @@ public class SlotGainServiceTests {
 		int count= slotGainService.slotGainCount();
 		assertEquals(count,9);
 	}
+	
+	@Test
+	void testAddingSlotGain() {
+		SlotGain new_gain = new SlotGain();
+		new_gain.setDate(LocalDate.of(2020, 3, 14));
+		new_gain.setAmount(100);
+		
+		Slotgame slotgame = new Slotgame();
+		slotgame.setName("Game");
+		slotgame.setJackpot(1000);
+		
+		Status status = new Status();
+		status.setName("REPAIR");
+		
+		SlotMachine slotmachine = new SlotMachine();
+		slotmachine.setSlotgame(slotgame);
+		slotmachine.setStatus(status);
+		Integer slotmachineid = slotmachine.getId();
+		new_gain.setSlotMachine(slotmachine);
+		
+		Collection<SlotGain> sampleSlotGains = new ArrayList<SlotGain>();
+		sampleSlotGains.add(new_gain);
+        when(slotGainRepo.findAll()).thenReturn(sampleSlotGains);
+		
+        
+		List<SlotGain> slotGains = StreamSupport.stream(this.slotGainService.findAll().spliterator(), false).collect(Collectors.toList());
+		SlotGain saved_slotGain = slotGains.get(slotGains.size()-1);
+		assertThat(saved_slotGain.getDate().equals(LocalDate.of(2020, 3, 14)));
+		assertThat(saved_slotGain.getAmount()==100);
+		assertThat(saved_slotGain.getSlotMachine().getSlotgame().getName().equals("Game"));
+		assertThat(saved_slotGain.getSlotMachine().getStatus().getName().equals("REPAIR"));
+
+	}
+	
+	/*
 	//INSERT INTO slotgains VALUES (1,'1',100,'2010-09-07',1);
 	@Test
 	void shouldFindSlotGains() {
 		List<SlotGain> slotgains = StreamSupport.stream(this.slotGainService.findAll().spliterator(), false).collect(Collectors.toList());
 		
 		SlotGain slotGain = EntityUtils.getById(slotgains, SlotGain.class, 2);
+<<<<<<< Updated upstream
 		assertTrue(slotGain.getDate().equals(LocalDate.of(2010, 9, 07)));
 		assertTrue(slotGain.getSlotMachine().equals(1));
 		assertTrue(slotGain.getAmount().equals(100));
 
+=======
+		assertThat(slotGain.getDate().equals(LocalDate.of(2010, 9, 07)));
+		assertThat(slotGain.getSlotMachine().equals(1));
+		assertThat(slotGain.getAmount().equals(100));
+>>>>>>> Stashed changes
 	}
+	*/
 }
