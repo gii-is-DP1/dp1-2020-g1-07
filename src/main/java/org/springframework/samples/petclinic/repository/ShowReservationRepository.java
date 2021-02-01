880c8e0cd9c103a3370d07b47106669de1a0c2d9
Antonio González Gómez <antoniogg696@gmail.com>
2021-02-01 19:22:30 +0100

    Added methods to ShowReservation repository and service

--- src/main/java/org/springframework/samples/petclinic/repository/ShowReservationRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/ShowReservationRepository.java
@@ -1,9 +1,24 @@
 package org.springframework.samples.petclinic.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.samples.petclinic.model.ShowReservation;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.samples.petclinic.model.ShowReservation;
+import org.springframework.stereotype.Repository;
+
+@Repository("showReservationRepository")
 public interface ShowReservationRepository extends CrudRepository<ShowReservation, Integer>{
 
+	@Query("SELECT DISTINCT r FROM ShowReservation r WHERE r.event.date LIKE :date% ORDER BY r.event.name")
+	List<ShowReservation> findShowresForDay(@Param("date") LocalDate date) throws DataAccessException;
 	
+	@Query("SELECT SUM(r.seats) FROM ShowReservation r WHERE r.event.id LIKE :id%")
+	Integer findReservedSeats(@Param("id") Integer id) throws DataAccessException;
+	
+	@Query("SELECT e.stage.capacity FROM Event s WHERE e.id LIKE :id%")
+	Integer findTotalSeats(@Param("id") Integer id) throws DataAccessException;
 }
--- src/main/java/org/springframework/samples/petclinic/service/ShowReservationService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ShowReservationService.java
@@ -1,13 +1,22 @@
 package org.springframework.samples.petclinic.service;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Client;
+import org.springframework.samples.petclinic.model.Game;
 import org.springframework.samples.petclinic.model.ShowReservation;
 import org.springframework.samples.petclinic.repository.ShowReservationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ShowReservationService {
 private  ShowReservationRepository showresRepo;
@@ -39,5 +48,15 @@ private  ShowReservationRepository showresRepo;
 
 	public  void delete(ShowReservation showres) { 
 		showresRepo.delete(showres);
+	}
+	
+	public Collection<ShowReservation> findShowresForDay(LocalDate date) throws DataAccessException{
+		log.info("Loading show reservations from DB for day: " + date.toString());
+		return showresRepo.findShowresForDay(date);
+	}
+	
+	public Integer findAvailableSeats(Integer eventId) throws DataAccessException{
+		log.info("Loading available seats from DB for event: " + eventId);
+		return showresRepo.findTotalSeats(eventId) - showresRepo.findReservedSeats(eventId);
 	}
 }
